fc3baccc12997acb0f73bd0d6458b22c881d224a
RONNY <mboyaronny41@gmail.com>
2025-09-16 10:25:55 +0300

    array reduce

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//sum all, batteries
+const totalBatteries = batteryBatches.reduce((sum, batch) => sum + batch, 0);
+//expose it globally so the test can see it
+if (typeof window !== 'undefined') {
+    window.totalBatteries = totalBatteries;
+
+}else {
+    global.totalBatteries = totalBatteries;
+}
